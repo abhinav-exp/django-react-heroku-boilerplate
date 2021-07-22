6278912ae596bcf87ae45fe701bb23d9da95055b
Abhinav <kabhinav482@gmail.com>
2021-07-22 12:53:52 +0530

    node on top

--- djproj/djproj/settings.py
+++ djproj/djproj/settings.py
@@ -57,7 +57,7 @@ import os
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, '../front/build')],
+        'DIRS': [os.path.join(BASE_DIR, '../build')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -122,7 +122,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = 'staticfiles/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, '../front/build/static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, '../build/static')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
